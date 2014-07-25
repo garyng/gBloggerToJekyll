91460f2c809e0893ace0b212b6ae36ec4535f5a1
GaryNg <garyng.zhongbo@gmail.com>
2014-07-26 01:40:31 +0800

    + Post converting function

--- gBloggerToJekyll/Program.cs
+++ gBloggerToJekyll/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -16,9 +18,10 @@ namespace gBloggerToJekyll
 	{
 		static void Main(string[] args)
 		{
-			BloggerHelper bh = new BloggerHelper("1063292627846-cklckmk7efnioeli841r29q98n8tika4.apps.googleusercontent.com", "zIJi0yOm0d3v54oPsu2jvvkF", "gBloggerToJekyll");
-			List<Blog> blogs = new List<Blog>();
+			BloggerManager bh = new BloggerManager("1063292627846-cklckmk7efnioeli841r29q98n8tika4.apps.googleusercontent.com", "zIJi0yOm0d3v54oPsu2jvvkF", "gBloggerToJekyll");
+			List<Blog> blogs = new List<Blog>();			
 
+			Console.ReadKey();
 			bh.Login();
 			//List blogs
 			blogs = bh.ListAllBlogs();
@@ -31,15 +34,33 @@ namespace gBloggerToJekyll
 			});
 
 			//Select blog
-			string blogId = blogs[0].Id;
+			Blog blog = blogs[1];
 
-			BlogPerUserInfo bpui = bh.GetBlogUserInfo(blogId);
+			BlogPerUserInfo bpui = bh.GetBlogUserInfo(blog.Id);
 			Console.WriteLine(bpui.UserId);
 			Console.WriteLine(bpui.Role);
-
+			
+			
+			//Kill if no admin
 			if (bpui.HasAdminAccess == true)
 			{
-				Console.WriteLine(bh.GetPageViews(blogId));
+				ConvertManager cm = new ConvertManager("_converted", "_tmp");
+				Console.WriteLine(bh.GetPageViews(blog.Id));
+				//bh.GetLivePosts(blog.Id).ForEach(item => Console.WriteLine(item.Title + " " + item.Url));
+				BloggerManager.PostInfo pi = new BloggerManager.PostInfo();
+				bh.GetLivePosts(blog.Id).ForEach(delegate(Post item)
+				{
+					pi = bh.GetPost(blog.Id, item.Id);
+					cm.SavePost(pi);
+					Console.WriteLine(pi.Title);
+				});
+
+				//BloggerManager.PostInfo pi = bh.GetPost(blog.Id, bh.GetLivePosts(blog.Id)[0].Id);
+				//pi.Tags.ForEach(item => Console.WriteLine(item));
+				//ConvertManager bcm = new ConvertManager("_converted","_temp");
+				//bcm.SavePost(pi);
+
+				//bh.GetDraftPosts(blog.Id).ForEach(item => Console.WriteLine("* " + item.Title));
 			}
 
 			Console.ReadKey();
@@ -47,7 +68,112 @@ namespace gBloggerToJekyll
 		}
 	}
 	
-	class BloggerHelper
+	class ConvertManager
+	{
+		string _strSaveFolderName;
+		string _strSaveFolderTempName;
+		public ConvertManager(string saveFolderName, string saveFolderTempName)
+		{
+			_strSaveFolderName = saveFolderName;
+			_strSaveFolderTempName = saveFolderTempName;
+			
+		}
+
+		public void SavePost(BloggerManager.PostInfo postInfo)
+		{
+			string filename = processFileName(postInfo.Url, postInfo.Published, ".md");
+			
+			string frontMatter = createPostFrontMatter(postInfo.Title, postInfo.AuthorName, postInfo.Tags);
+			//string fmFilename = saveToFile(frontMatter, filename, ".fm", _strSaveFolderTempName);
+
+			string content = postInfo.Content;
+			string contentFilename = saveToFile(content, filename, ".content", _strSaveFolderTempName);
+
+			content = convertPostContent(contentFilename);
+
+			saveToFile(frontMatter + content, filename, "", _strSaveFolderName);
+			//Directory.Delete(_strSaveFolderTempName, true);
+		}
+		
+		private string saveToFile(string content, string filename, string extension, string folder)
+		{
+			if (!Directory.Exists(folder))
+			{
+				Directory.CreateDirectory(folder);
+			}
+			string savePath = folder +  @"\" + filename + extension;
+			using (StreamWriter sw = new StreamWriter(savePath,false))
+			{
+				sw.Write(content);
+				sw.Flush();
+			};
+			return savePath;
+		}
+
+		private string convertPostContent(string filename)
+		{
+			string processName = "html2text.exe";
+			string processArgs = " \"" + filename + "\" -b 0";
+
+			ProcessStartInfo psi = new ProcessStartInfo(processName, processArgs);
+			psi.RedirectStandardOutput = true;
+
+			Process proc = new Process();
+			proc.StartInfo = psi;
+
+			psi.UseShellExecute = false;
+			proc.Start();
+
+			string outputString = "";
+			proc.WaitForExit(2000);
+			using (StreamReader sr = new StreamReader(proc.StandardOutput.BaseStream))
+			{
+				outputString = sr.ReadToEnd();
+			}
+			return outputString;
+		}
+
+		private string processFileName(string postUrl, DateTime? date, string extension)
+		{
+			string postName = Path.GetFileNameWithoutExtension(new Uri(postUrl).AbsolutePath).Trim().ToLower();
+			string postDate = date.HasValue ? date.Value.ToString("yyyy-MM-dd").Trim() : DateTime.Now.ToString("yyyy-MM-dd").Trim();
+			string postFilename = postDate + "-" + postName + extension;
+			return postFilename;
+		}
+
+		private string createPostFrontMatter(string Title, string AuthorName, List<string> Tags)
+		{
+			string tag = "---";
+			string layout = "post";
+			string title = escapeString(Title);
+			string author = escapeString(AuthorName);
+			string tags = "[" + string.Join(",", Tags.Select(item=>escapeString(item))) + "]";
+
+			string[] frontMatter = {
+									   tag,
+									   joinFrontMatterPropValue("layout",layout),
+									   joinFrontMatterPropValue("title",title),
+									   joinFrontMatterPropValue("author",author),
+									   joinFrontMatterPropValue("tags",tags),
+									   tag,
+									   Environment.NewLine
+								   };
+			return string.Join(Environment.NewLine, frontMatter);
+		}
+
+		private string joinFrontMatterPropValue(string property, string value)
+		{
+			return property + ": " + value;
+		}
+
+		private string escapeString(string str)
+		{
+			str = "'" + str.Replace("'", "''") + "'";
+			return str;
+		}
+	}
+
+	class BloggerManager
 	{
 		ClientSecrets _csAppSec = new ClientSecrets();
 		UserCredential _ucUser = null;
@@ -55,7 +181,18 @@ namespace gBloggerToJekyll
 		FileDataStore _fdsStore = null;
 		string _strAppName = "";
 
-		public BloggerHelper(string clientId, string clientSec,string appName)
+		public struct PostInfo
+		{
+			public string Title;
+			public string Content;
+			public string Url;
+			public string AuthorName;
+			public DateTime? Published;
+			public List<string> Tags;
+
+		}
+
+		public BloggerManager(string clientId, string clientSec,string appName)
 		{
 			_csAppSec = new ClientSecrets
 			{
@@ -69,19 +206,8 @@ namespace gBloggerToJekyll
 
 		public void Login()
 		{
-			_ucUser = GoogleWebAuthorizationBroker.AuthorizeAsync(
-				_csAppSec,
-				new[] { BloggerService.Scope.Blogger },
-				"user",
-				CancellationToken.None,
-				_fdsStore
-				).Result;
-
-			_bsBlogger = new BloggerService(new BaseClientService.Initializer
-				{
-					HttpClientInitializer = _ucUser,
-					ApplicationName = _strAppName
-				});
+			_ucUser = BloggerHelper.createCredential(_csAppSec, _fdsStore);
+			_bsBlogger = BloggerHelper.createService(_ucUser, _strAppName);
 		}
 
 		public void Logout()
@@ -93,26 +219,117 @@ namespace gBloggerToJekyll
 
 		public List<Blog> ListAllBlogs()
 		{
-			return listAllBlogs(_bsBlogger);
+			return BloggerHelper.listAllBlogs(_bsBlogger);
 		}
 
 		public BlogPerUserInfo GetBlogUserInfo(string blogId)
 		{
-			return getBlogUserInfo(_bsBlogger, blogId);
+			return BloggerHelper.getBlogUserInfo(_bsBlogger, blogId);
 		}
 
 		public long? GetPageViews(string blogId)
 		{
-			return getPageViews(_bsBlogger, blogId, PageViewsResource.GetRequest.RangeEnum.All)[0].Count;
+			return BloggerHelper.getPageViews(_bsBlogger, blogId, PageViewsResource.GetRequest.RangeEnum.All)[0].Count;
 		}
 
-		private List<Pageviews.CountsData> getPageViews(BloggerService bs, string blogId, PageViewsResource.GetRequest.RangeEnum range)
+		public List<Post> GetLivePosts(string blogId)
+		{
+			return BloggerHelper.getPostList(_bsBlogger, blogId, PostsResource.ListRequest.StatusEnum.Live);
+		}
+
+		public List<Post> GetDraftPosts(string blogId)
+		{
+			return BloggerHelper.getPostList(_bsBlogger, blogId, PostsResource.ListRequest.StatusEnum.Draft);
+		}
+
+		public BloggerManager.PostInfo GetPost(string blogId, string postId)
+		{
+			Post p = BloggerHelper.getPost(_bsBlogger, blogId, postId);
+			PostInfo pi = new PostInfo();
+			pi.AuthorName = p.Author.DisplayName;
+			pi.Content = p.Content;
+			pi.Published = p.Published;
+			pi.Title = p.Title;
+			pi.Url = p.Url;
+			pi.Tags = p.Labels == null? new List<string>() : p.Labels.ToList();
+			return pi;
+		}
+
+	}
+
+	static class BloggerHelper
+	{
+		public static UserCredential createCredential(ClientSecrets cs, FileDataStore fds)
+		{
+			UserCredential uc = GoogleWebAuthorizationBroker.AuthorizeAsync(
+				cs,
+				new[] { BloggerService.Scope.Blogger },
+				"user",
+				CancellationToken.None,
+				fds
+				).Result;
+			return uc;
+		}
+
+		public static BloggerService createService(UserCredential uc, string appName)
+		{
+			BloggerService bs = new BloggerService(new BaseClientService.Initializer
+				{
+					HttpClientInitializer = uc,
+					ApplicationName = appName
+				});
+			return bs;
+		}
+
+		public static Post getPost(BloggerService bs, string blogId, string postId)
+		{
+			PostsResource.GetRequest req = bs.Posts.Get(blogId, postId);
+			req.View = PostsResource.GetRequest.ViewEnum.ADMIN;
+
+			Post p = req.Execute();
+			return p;
+		}
+
+		public static List<Post> getPostList(BloggerService bs, string blogId, PostsResource.ListRequest.StatusEnum status)
+		{
+			PostsResource.ListRequest req = bs.Posts.List(blogId);
+			req.View = PostsResource.ListRequest.ViewEnum.ADMIN;
+			req.FetchBodies = false;
+			req.FetchImages = false;
+			req.Status = status;
+
+			List<Post> listOfPost = new List<Post>();
+			string firstToken = "";
+
+			while (true)
+			{
+				PostList posts = req.Execute();
+				req.PageToken = posts.NextPageToken;
+				if (firstToken == "")
+				{
+					firstToken = posts.NextPageToken;
+				}
+				else if (firstToken != "" && posts.NextPageToken == firstToken)
+				{
+					break;
+				}
+				if (posts.Items != null)
+				{
+					posts.Items.ToList().ForEach(item => listOfPost.Add(item));
+				}
+			}
+
+			return listOfPost;
+
+		}
+
+		public static List<Pageviews.CountsData> getPageViews(BloggerService bs, string blogId, PageViewsResource.GetRequest.RangeEnum range)
 		{
 			if (bs == null || getBlogUserInfo(bs, blogId).HasAdminAccess != true)
 			{
 				return null;
 			}
-			
+
 			PageViewsResource.GetRequest req = bs.PageViews.Get(blogId);
 			req.Range = range;
 
@@ -120,20 +337,20 @@ namespace gBloggerToJekyll
 			return pv.Counts.ToList();
 		}
 
-		private BlogPerUserInfo getBlogUserInfo(BloggerService bs, string blogId)
+		public static BlogPerUserInfo getBlogUserInfo(BloggerService bs, string blogId)
 		{
 			if (bs == null)
 			{
 				return null;
 			}
 
-			BlogUserInfosResource.GetRequest req = bs.BlogUserInfos.Get("self",blogId);
+			BlogUserInfosResource.GetRequest req = bs.BlogUserInfos.Get("self", blogId);
 			BlogUserInfo bui = req.Execute();
 
 			return bui.BlogUserInfoValue;
 		}
 
-		private List<Blog> listAllBlogs(BloggerService bs)
+		public static List<Blog> listAllBlogs(BloggerService bs)
 		{
 			if (bs == null)
 			{
